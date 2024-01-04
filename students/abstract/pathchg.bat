d476a5e7d0ec1ba5713707c93c0ed4a4e5ae56a6
TienTsinWong <ttwong@acm.org>
2024-01-04 13:36:32 +0800

    add students

---
+++ students/abstract/pathchg.bat
@@ -0,0 +1,5 @@
+C:\develop\Python36\python.exe  pathchg.py  "/~ttwong/icon/" "/icon/"
+C:\develop\Python36\python.exe  pathchg.py  "/~ttwong/students/figure/" "../figure/"
+C:\develop\Python36\python.exe  pathchg.py  "/~ttwong/papers/" "papers/"
+C:\develop\Python36\python.exe  pathchg.py  "<!--#include virtual=\"/~ttwong/footer1.html\"-->" ""
+
---
+++ students/title/pathchg.bat
@@ -0,0 +1,3 @@
+C:\develop\Python36\python.exe  pathchg.py  "./papers/_author" "papers/_author"
+
+
